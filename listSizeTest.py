47dc2858efc0e1c46ca67889e7f2483cea3c69e6
Brandon Peebles <peeblesbrandon@gmail.com>
2020-07-01 19:23:34 -0400

    add demonstration of pop method

--- listSizeTest.py
+++ listSizeTest.py
@@ -1,9 +1,16 @@
 import sys
 
 data = []
+print("####### TESTING APPEND METHOD #######")
 for k in range(50):
     a = len(data)
     b = sys.getsizeof(data)
     print('Length: {0:3d}; Size in bytes: {1:4d}'.format(a, b))
     data.append(None)
 
+print("####### TESTING POP METHOD #######")
+for k in range(50):
+    data.pop()
+    a = len(data)
+    b = sys.getsizeof(data)
+    print('Length: {0:3d}; Size in bytes: {1:4d}'.format(a, b))
